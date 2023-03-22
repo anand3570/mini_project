4bee57599e07bf30090932b87ebcad0a1731bd53
anand3570 <121782244+anand3570@users.noreply.github.com>
2023-03-22 23:06:20 +0530

    Add files via upload

---
+++ src/foodordering/TestFoodOrder.java
@@ -0,0 +1,13 @@
+package foodordering;
+
+public class TestFoodOrder {
+	public static void main(String[] args)
+    {
+        FoodOrder fo = new FoodOrder();
+
+        fo.displayMenu();
+
+        fo.order();
+        //lets compile and test
+    }
+}
